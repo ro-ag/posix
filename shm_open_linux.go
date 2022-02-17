abecf268c7b7188a82cc8a1c51b536dbc242c9c1
Rodrigo Agurto <roagto@gmail.com>
2022-02-17 06:22:12 -0300

    fix shm open path

--- shm_open_linux.go
+++ shm_open_linux.go
@@ -68,7 +68,7 @@ func unlinkat(dirfd int, path string, flags int) (err error) {
 
 //goland:noinspection GoSnakeCaseUsage
 const (
-	prefix        = "/dev/shmTable/"
+	prefix        = "/dev/shm/"
 	_SYS_OPENAT   = 257
 	_SYS_UNLINKAT = 263
 	_AT_FDCWD     = -0x64
